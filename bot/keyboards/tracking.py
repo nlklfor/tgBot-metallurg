f1941748faf82d151461cb8ce25f389bf5064b54
nlklfor <nik.slepcov1771@gmail.com>
2026-04-24 00:50:55 +0200

    fix: fixed np tracking

--- bot/keyboards/tracking.py
+++ bot/keyboards/tracking.py
@@ -4,11 +4,11 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def tracking_keyboard(ttn: str, order_number: str) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(inline_keyboard=[[
         InlineKeyboardButton(
-            text="[ ВІДСТЕЖИТИ НА НП ]",
+            text="[ TRACK ON NP ]",
             url=f"https://novaposhta.ua/tracking/?cargo_number={ttn}",
         ),
         InlineKeyboardButton(
-            text="[ ДЕТАЛІ ЗАМОВЛЕННЯ ]",
+            text="[ ORDER DETAILS ]",
             callback_data=f"order_details:{order_number}",
         ),
     ]])
@@ -17,7 +17,7 @@ def tracking_keyboard(ttn: str, order_number: str) -> InlineKeyboardMarkup:
 def no_ttn_keyboard(order_number: str) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(inline_keyboard=[[
         InlineKeyboardButton(
-            text="[ ДЕТАЛІ ЗАМОВЛЕННЯ ]",
+            text="[ ORDER DETAILS ]",
             callback_data=f"order_details:{order_number}",
         ),
     ]])
